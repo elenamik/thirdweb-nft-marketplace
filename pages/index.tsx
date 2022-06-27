641da0b003ac86882ec7367771eee4787ce1c86e
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-06-27 12:15:11 -0400

    small manicure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -8,9 +6,7 @@ const Home: NextPage = () => {
     <div className={styles.container}>
 
         <div className="text-3xl">
-
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-
         </div>
     </div>
   )
