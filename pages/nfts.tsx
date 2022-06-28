ccf13f8aa0d9e14d34d278e6af63c1cd3e9bdc5a
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-06-28 13:14:51 -0400

    route to my NFTs

--- pages/nfts.tsx
+++ pages/nfts.tsx
@@ -38,7 +38,7 @@ const formatDisplayAddress = (address:string):string => {
  * // switch to server props
  */
 
-const Home: NextPage<{allNFTrees: string, listings: string }> = (props) => {
+const Nfts: NextPage<{allNFTrees: string, listings: string }> = (props) => {
   const connectWithMetamask = useMetamask()
   const address = useAddress()
   const disconnectWallet = useDisconnect()
@@ -52,30 +52,9 @@ const Home: NextPage<{allNFTrees: string, listings: string }> = (props) => {
 
   return (
     <div id="container" className="font-roboto">
-      <div id="hero" className="bg-slate-100 pt-6 px-6 pb-2 flex flex-row justify-between">
-        <span id="hero-logo" className="text-5xl font-extrabold text-left text-slate-700 font-josephin font-semibold">ThirdSea</span>
-        <div id="nav" className="flex flex-row">
-          <Link href={'/nfts'}>
-            My Collection
-          </Link>
-          {!address
-            ? <button onClick={connectWithMetamask} id="wallet" className="bg-slate-700 text-slate-50 text-md font-medium px-4 h-12 rounded-3xl">Connect Wallet</button>
-            : <div id="account" className="flex align-middle">
-                <button onClick={disconnectWallet} id="disconnect" className="bg-slate-700 text-slate-50 text-md font-medium px-4 h-12 rounded-3xl">Disconnect Wallet</button>
-                <div className="p-2 text-lg text-slate-700 font-medium">
-                  <span className="text-3xl font-normal">|</span>
-                  <span id="address" className="p-2">{formatDisplayAddress(address)}</span>
-
-                </div>
-              </div>
-          }
-          </div>
-      </div>
-      <div>
-        {viewListings}
-      </div>
+   hi
     </div>
   )
 }
 
-export default Home
+export default Nfts
