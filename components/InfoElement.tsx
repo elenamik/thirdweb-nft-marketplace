4c4695cd4e7a3330b540a134ef670cbacc7b81f3
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-06-29 14:24:27 -0400

    more linting

--- components/InfoElement.tsx
+++ components/InfoElement.tsx
@@ -1,9 +1,14 @@
-import * as React from 'react'
+import * as React from "react";
 
-export const InfoElement: React.FC<{name: string, data:string}> = ({ name, data }) => {
-  return <div className="p-1 text-left">
-  <span className="font-bold">{name}:</span> {data}
-</div>
-}
+export const InfoElement: React.FC<{ name: string; data: string }> = ({
+  name,
+  data,
+}) => {
+  return (
+    <div className="p-1 text-left">
+      <span className="font-bold">{name}:</span> {data}
+    </div>
+  );
+};
 
-export default InfoElement
+export default InfoElement;
