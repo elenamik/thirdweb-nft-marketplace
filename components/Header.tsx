efc76576b312226fb6a89cac8ab447a835ad0274
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-06-29 14:24:18 -0400

    linting

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,36 +1,60 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-import * as React from 'react'
-import { useMetamask, useAddress, useDisconnect } from '@thirdweb-dev/react'
-import { useRouter } from 'next/router'
-import { formatDisplayAddress } from '../web3utils'
+import * as React from "react";
+import { useMetamask, useAddress, useDisconnect } from "@thirdweb-dev/react";
+import { useRouter } from "next/router";
+import { formatDisplayAddress } from "../web3utils";
 
-const Header: React.FC = (props) => {
-  const connectWithMetamask = useMetamask()
-  const address = useAddress()
-  const disconnectWallet = useDisconnect()
-  const router = useRouter()
+const Header: React.FC = () => {
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+  const disconnectWallet = useDisconnect();
+  const router = useRouter();
 
   return (
-      <div id="hero" className="bg-slate-100 pt-6 px-6 pb-2 flex flex-row justify-between">
-        <span onClick={() => { router.push('/') }} id="hero-logo" className="text-5xl font-extrabold text-left text-slate-700 font-josephin font-semibold">ThirdSea</span>
-        <div id="nav" className="flex flex-row">
-          <Link href={'/nfts'}>
-            My Collection
-          </Link>
-          {!address
-            ? <button onClick={connectWithMetamask} id="wallet" className="bg-slate-700 text-slate-50 text-md font-medium px-4 h-12 rounded-3xl">Connect Wallet</button>
-            : <div id="account" className="flex align-middle">
-                <button onClick={disconnectWallet} id="disconnect" className="bg-slate-700 text-slate-50 text-md font-medium px-4 h-12 rounded-3xl">Disconnect Wallet</button>
-                <div className="p-2 text-lg text-slate-700 font-medium">
-                  <span className="text-3xl font-normal">|</span>
-                  <span id="address" className="p-2">{formatDisplayAddress(address)}</span>
-                </div>
-              </div>
-          }
+    <div
+      id="hero"
+      className="flex flex-row justify-between bg-slate-100 px-6 pt-6 pb-2"
+    >
+      <span
+        onClick={() => {
+          router.push("/");
+        }}
+        id="hero-logo"
+        className="text-left font-josephin text-5xl font-extrabold font-semibold text-slate-700"
+      >
+        ThirdSea
+      </span>
+      <div id="nav" className="flex flex-row">
+        <Link href={"/nfts"}>My Collection</Link>
+        {!address ? (
+          <button
+            onClick={connectWithMetamask}
+            id="wallet"
+            className="text-md h-12 rounded-3xl bg-slate-700 px-4 font-medium text-slate-50"
+          >
+            Connect Wallet
+          </button>
+        ) : (
+          <div id="account" className="flex align-middle">
+            <button
+              onClick={disconnectWallet}
+              id="disconnect"
+              className="text-md h-12 rounded-3xl bg-slate-700 px-4 font-medium text-slate-50"
+            >
+              Disconnect Wallet
+            </button>
+            <div className="p-2 text-lg font-medium text-slate-700">
+              <span className="text-3xl font-normal">|</span>
+              <span id="address" className="p-2">
+                {formatDisplayAddress(address)}
+              </span>
+            </div>
           </div>
+        )}
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
