27834546e9c921b61eb9e147e68e18c3ada48717
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-06-29 17:29:11 -0400

    buy works

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Header from '../components/Header'
-import * as React from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ThirdwebProvider } from '@thirdweb-dev/react'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "../components/Header";
+import * as React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
-function MyApp ({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient()
+function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
 
   return (
-      <ThirdwebProvider desiredChainId={4}>
-          <QueryClientProvider client={queryClient}>
-          <Header />
-            <Component {...pageProps} />
-          </QueryClientProvider>
-        </ThirdwebProvider>
-  )
+    <ThirdwebProvider desiredChainId={4}>
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
