abc8fc1078d2d0614b85c8685d402f3239ee09c8
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-06-30 13:42:56 -0400

    fixed access control

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    // Extending webback config to support TypeGraphQL - enabling top level await
+    if (!config.experiments) {
+      config.experiments = {};
+    }
+    config.experiments.topLevelAwait = true;
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,35 @@
+import "reflect-metadata"; // must import before importing type-graphql resolvers
 import { ApolloServer } from "apollo-server-micro";
+import {
+  buildSchema,
+  Resolver,
+  Query,
+  Arg,
+  ObjectType,
+  Field,
+  ID,
+} from "type-graphql";
 
-const server = new ApolloServer({});
+@ObjectType()
+export class Dog {
+  @Field(() => ID)
+  name: string;
+}
+
+@Resolver(Dog)
+export class DogResolver {
+  @Query(() => [Dog])
+  dogs(): Dog[] {
+    return [{ name: "Bo" }, { name: "T-rex" }];
+  }
+}
+
+const schema = await buildSchema({
+  resolvers: [DogResolver],
+});
+const server = new ApolloServer({ schema });
+
+const startServer = server.start();
 
 // tell next.js to not parse the body, since we are using GQL
 export const config = {
@@ -9,9 +38,25 @@ export const config = {
   },
 };
 
-const startServer = server.start();
-
 export default async function handler(req: any, res: any) {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://studio.apollographql.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD"
+  );
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+
   await startServer;
   await server.createHandler({ path: "/api/graphql" })(req, res);
 }
