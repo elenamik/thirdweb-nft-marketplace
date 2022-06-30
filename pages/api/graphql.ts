a60dd96f424dbb058260d947b3d5f92b9162eff9
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-06-30 14:11:54 -0400

    nexus works

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,62 +1,41 @@
-import "reflect-metadata"; // must import before importing type-graphql resolvers
 import { ApolloServer } from "apollo-server-micro";
-import {
-  buildSchema,
-  Resolver,
-  Query,
-  Arg,
-  ObjectType,
-  Field,
-  ID,
-} from "type-graphql";
+import { queryType, makeSchema } from "nexus";
 
-@ObjectType()
-export class Dog {
-  @Field(() => ID)
-  name: string;
-}
-
-@Resolver(Dog)
-export class DogResolver {
-  @Query(() => [Dog])
-  dogs(): Dog[] {
-    return [{ name: "Bo" }, { name: "T-rex" }];
-  }
-}
-
-const schema = await buildSchema({
-  resolvers: [DogResolver],
+const Query = queryType({
+  definition(t) {
+    t.string("hello", { resolve: () => "hello world!" });
+  },
 });
-const server = new ApolloServer({ schema });
+const schema = makeSchema({
+  types: [Query],
+});
 
-const startServer = server.start();
+const server = new ApolloServer({
+  schema,
+});
 
-// tell next.js to not parse the body, since we are using GQL
-export const config = {
+type Config = {
+  api: {
+    externalResolver?: boolean;
+    bodyParser?:
+      | boolean
+      | {
+          sizeLimit: string;
+        };
+  };
+};
+
+export const config: Config = {
   api: {
     bodyParser: false,
   },
 };
 
-export default async function handler(req: any, res: any) {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://studio.apollographql.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD"
-  );
-  if (req.method === "OPTIONS") {
-    res.end();
-    return false;
-  }
+const startServer = server.start();
 
+export default async function handler(req: any, res: any) {
   await startServer;
-  await server.createHandler({ path: "/api/graphql" })(req, res);
+  await server.createHandler({
+    path: "/api/graphql",
+  })(req, res);
 }
