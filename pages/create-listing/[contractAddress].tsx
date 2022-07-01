450cc44675f4480f56dcfce74e93cfc5c1598643
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-07-01 12:30:52 -0400

    listing works woo

--- pages/create-listing/[contractAddress].tsx
+++ pages/create-listing/[contractAddress].tsx
@@ -1,14 +1,13 @@
 import { NextPage, NextPageContext } from "next";
 
 import * as React from "react";
-import { getNftMetadata, OwnedNft } from "@alch/alchemy-sdk";
+import { getNftMetadata, Nft, NftTokenType } from "@alch/alchemy-sdk";
 import { useMarketplace } from "@thirdweb-dev/react";
 import { MarketPlaceContractAddress } from "../../config/contractAddresses";
 import { targetChain } from "../../config/targetChain";
 import { NATIVE_TOKEN_ADDRESS } from "@thirdweb-dev/sdk";
 import { useRouter } from "next/router";
 import { alchemy } from "../../config/alchemy";
-import { NftTokenType } from "@alch/alchemy-sdk/dist/src/types/types";
 import NftCard from "../../components/NftCard";
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -27,27 +26,42 @@ export async function getServerSideProps(context: NextPageContext) {
 
 const CreateListingPage: NextPage<{ data: string }> = (props) => {
   const router = useRouter();
-  const NFT = JSON.parse(props.data);
+  const NFT: Nft = JSON.parse(props.data);
   console.log("DATA", NFT);
-  const { contractAddress } = router.query as { contractAddress: string };
+  const [price, setPrice] = React.useState<string>("1");
+  const [creating, setCreating] = React.useState<boolean>(false);
 
   const marketplace = useMarketplace(MarketPlaceContractAddress[targetChain]);
 
-  const createListing = async (nft: OwnedNft, listingPrice: string) => {
+  const handlePriceChange = (event: { target: { value: string } }) => {
+    const re = /^[0-9]+\.?[0-9]*$/;
+
+    const val = event.target.value;
+    if (val === "" || re.test(val)) {
+      setPrice(val);
+    }
+  };
+
+  const createListing = async () => {
     const listing = {
-      assetContractAddress: nft.contract.address,
-      tokenId: nft.tokenId,
+      assetContractAddress: NFT.contract.address,
+      tokenId: NFT.tokenId,
       startTimestamp: new Date(),
       listingDurationInSeconds: 86400,
       quantity: 1,
       currencyContractAddress: NATIVE_TOKEN_ADDRESS,
-      buyoutPricePerToken: listingPrice,
+      buyoutPricePerToken: price.toString(),
     };
 
-    const tx = await marketplace!.direct.createListing(listing);
-    if (tx.id) {
-      const listingId = tx.id; // the id of the newly created listing
-      router.push(`/listing/${listingId}`);
+    setCreating(true);
+    try {
+      const tx = await marketplace!.direct.createListing(listing);
+      if (tx.id) {
+        const listingId = tx.id; // the id of the newly created listing
+        router.push(`/listing/${listingId}`);
+      }
+    } catch (error: any) {
+      setCreating(false);
     }
   };
 
@@ -64,15 +78,34 @@ const CreateListingPage: NextPage<{ data: string }> = (props) => {
           </span>
           <div>
             <input
-              type="number"
+              type="string"
               className="max-w-sm rounded-2xl bg-slate-300 p-2"
-              value={1}
+              value={price}
+              onChange={handlePriceChange}
             />
             {"  "}⧫
           </div>
-          <button className="m-1 mt-6 rounded-3xl bg-slate-900 p-4 text-center text-2xl font-semibold text-slate-200 transition ease-in-out hover:border-4 hover:border-slate-200 hover:shadow-lg  active:scale-105">
-            Execute
-          </button>
+          {creating ? (
+            <>
+              <button
+                disabled={true}
+                onClick={createListing}
+                className="m-1 mt-6 rounded-3xl bg-slate-900 p-4 text-center text-2xl font-semibold text-slate-200 transition ease-in-out hover:border-4 hover:border-slate-200 hover:shadow-lg  active:scale-105"
+              >
+                Creating Listing...
+              </button>
+              <div className="text-sm text-slate-800">
+                We will reroute you when the transaction is completed
+              </div>
+            </>
+          ) : (
+            <button
+              onClick={createListing}
+              className="m-1 mt-6 rounded-3xl bg-slate-900 p-4 text-center text-2xl font-semibold text-slate-200 transition ease-in-out hover:border-4 hover:border-slate-200 hover:shadow-lg  active:scale-105"
+            >
+              Execute
+            </button>
+          )}
         </div>
       </div>
     </div>
