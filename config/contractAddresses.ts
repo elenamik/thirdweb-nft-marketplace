590e265c34f490ac97a47d6ca455164b8305af0d
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-07-22 11:59:31 -0400

    rm consoles

--- config/contractAddresses.ts
+++ config/contractAddresses.ts
@@ -5,13 +5,16 @@ interface IAddresses {
   [key: string]: { [key: string]: string };
 }
 
+/***
+ * Add your addresses + networks here.
+ * To tell your app where to point to, change the targetChainId
+ */
 const ADDRESSES: IAddresses = {
   [ChainId.Goerli]: {
     Marketplace: "0x14f3d32B90Ede9922Fd7EE4FDfD3AabbEA423aD0",
   },
 };
 
-console.log(typeof ADDRESSES);
 export const readAppContractAddresses = (name: string) => {
   return ADDRESSES[targetChainId][name];
 };
